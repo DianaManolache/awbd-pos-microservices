857d3e41dcc3d210c7c2867e3c415f47aff80545
Diana Manolache <dmanolache@adobe.com>
2026-07-05 22:44:39 +0300

    Repository si DTO-uri promotie

---
+++ src/main/java/ro/facultate/pos/repository/PromotieRepository.java
@@ -0,0 +1,6 @@
+package ro.facultate.pos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ro.facultate.pos.entity.Promotie;
+
+public interface PromotieRepository extends JpaRepository<Promotie, Long> {}
